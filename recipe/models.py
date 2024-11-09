3e895e78ad6e1e5dad47d884a796cf0057dc7ce3
Ayisha Sandiford <ish.equal@gmail.com>
2024-11-09 03:22:40 +0000

    Allow recipes to be create with the same information

--- recipe/models.py
+++ recipe/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Recipe(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_owner")
-    title = models.CharField(max_length=50, unique=True)
-    ingredients = models.TextField(max_length=10000, null=False, blank=False, unique=True, help_text="List your ingredients here, separated by commas or new lines")
-    instructions = models.TextField(max_length=10000, null=False, blank=False, unique=True, help_text="Step-by-step instructions")
+    title = models.CharField(max_length=50,)
+    ingredients = models.TextField(max_length=10000, null=False, blank=False, help_text="List your ingredients here, separated by commas or new lines")
+    instructions = models.TextField(max_length=10000, null=False, blank=False, help_text="Step-by-step instructions")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
