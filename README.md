61713c8d39d4b4b6d31687ce30fb62ae698329f5
Ayisha Sandiford <ish.equal@gmail.com>
2024-11-13 07:50:35 +0000

    update readme file

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # Treasure Your Recipe
-![Screenshot of Hero Section]()
-> [Live Site]()
+![Screenshot of Hero Section](readme/screenshot_responsive.png)
+> [Live Site](https://treasure-your-recipe-812ee6481b1f.herokuapp.com/)
 
 ## Project Overview
 This project was created due to my love for cooking. My family has hundreds for secret recipes sent down from generations. Saving these recipes or as i like to call it "treasuring these recipes' is very important to me. This gave me the idea to create an app that will allow me to store these recipes all these fantastic recipes in one place so you can never lose them.
 
 ## Wireframes
 
+![Wireframe of Home Page](wireframe/home-page.png)
 - Landing page
 
+![Wireframe of User Dashboard](wireframe/dashboard.png)
 - Dashboard
 
+![Wireframe of Create Page](wireframe/create-page.png)
 - Create Recipe Page
 
 ## User Stories
-> [Link here to user Stories on github]()
+> [Link here to user Stories on github](https://github.com/AyishaSandiford/treasure-your-recipe-pp4/issues?q=is%3Aissue+is%3Aclosed)
 
 ## Features
 <details>
@@ -30,11 +33,48 @@ This project was created due to my love for cooking. My family has hundreds for 
 ![Screenshot of The Register Form]()
 
 </details>
+<br>
+<details>
+<summary>Screenshot of The Dashboard</summary>
+
+![Screenshot of The Register Form]()
+
+</details>
+<br>
+<details>
+<summary>Screenshot of The Create recipe Page</summary>
+
+![Screenshot of The Register Form]()
+
+</details>
+<br>
+<details>
+<summary>Screenshot of The Edit Page</summary>
+
+![Screenshot of The Register Form]()
+
+</details>
+<br>
+<details>
+<summary>Screenshot of The Delete message</summary>
+
+![Screenshot of The Register Form]()
+
+</details>
 
 
 ## Future Features
-- The ability to add an image of the final dish to your createde recipe
-- A feature to share your recipes to other cooks like yourself
+This application was created with minimal functionality (MVP). Just enought to 
+present to the client to help get a general idea of sites functionality.
+The intendtion of this application is to continue to build on it allowing
+more features in the future.
+See some of the Furtureideas listed below:
+
+- The ability to add an image of the final dish to your created recipe
+- Share your recipes with others 
+- Other users can commit, like or dislike your recipe
+- When creating a recipe and you accidently lose the page, what was entered wi;; be save to draft so you can contiune at and not lose you information.
+- Choose from a list of food cartaegories
 
 ## Langueges Used
 - HTML5
@@ -43,7 +83,7 @@ This project was created due to my love for cooking. My family has hundreds for 
 - PYTHON
 
 ## Agile Deveploment 
-The project was built using Agile...
+The project was built following Agile Principle. Epic stories were create and placed into todo's and draged across until completion.
 
 ## Testing
 The testing of this site was done manually. See table below
@@ -51,10 +91,10 @@ The testing of this site was done manually. See table below
 | Test                                  | Action                                                                                       | Result |
 | ------------------------------------- | -------------------------------------------------------------------------------------------- | ------ |
 | URL loads                             | Enter the website URL into a browser and press Enter.                                        | PASS   |
-| Text              | Text         | PASS   |
-| Text                  | Text            | PASS   |
-| Text   | Text       | PASS   |
-| Text   | Text | PASS   |
+| Create a recipe              | Click the Create recipe button        | PASS   |
+| Delete a recipe                  | Click the deleteb button  a message will appear askin g if you are sure you want to delete      | PASS   |
+| Edit a recipe  | Click the edit button and edit a perviously created recipe. ckick updae     | PASS   |
+| View   | Using the recipe id you can view one individual recipe | PASS   |
 
 
 
--- recipe/views.py
+++ recipe/views.py
@@ -5,24 +5,25 @@ from . import forms
 from django.shortcuts import redirect
 from django.contrib import messages
 
-"""
-    Returns the Authorize user's list of recipes.
-
-    This view returns the dashboard template to the user
-    displaying all of the user's created recipes
-
-    Parameters:
-    object that represents the client's request.
-
-    Returns:
-    HttpResponse:
-
-    """
-
 
 # Create your views here.
 @login_required
 def get_dashboard_view(request):
+    """
+    Renders the dashboard view with recipes specific to the currently logged-in user.
+
+    This view retrieves all recipes associated with the current user, filters them by the user's ID, 
+    and displays them on the dashboard page.
+
+    Parameters:
+    request (HttpRequest): The HTTP request object containing metadata about the request.
+
+    Returns:
+    HttpResponse: The rendered dashboard HTML template with the filtered list of recipes.
+
+    Note:
+    The user must be logged in to access this view.
+    """
     currently_logged_in_user_id = request.user.id
     all_recipes = Recipe.objects.all()
     filtered_recipes = all_recipes.filter(user=currently_logged_in_user_id)
@@ -37,6 +38,26 @@ def get_dashboard_view(request):
 
 @login_required
 def Create(request):
+    """
+    Handles the creation of a new recipe for the currently logged-in user.
+
+    If the request method is POST, this view processes the submitted recipe form. 
+    If the form is valid, it saves the new recipe with the current user's ID and 
+    displays a success message. After successful creation, it redirects the user 
+    to the dashboard view. If the request method is not POST, an empty recipe form 
+    is displayed.
+
+    Parameters:
+    request (HttpRequest): The HTTP request object containing metadata about the request.
+
+    Returns:
+    HttpResponse: If the request method is GET, renders the recipe creation template 
+                  with an empty form. If the form is submitted and valid (POST), 
+                  redirects to the dashboard view after saving the recipe.
+
+    Note:
+    The user must be logged in to access this view.
+    """
     if request.method == 'POST':
         form = forms.RecipeForm(request.POST)
         if form.is_valid():
@@ -54,6 +75,28 @@ def Create(request):
 
 @login_required
 def Edit(request, recipe_id):
+    """
+    Handles editing an existing recipe for the currently logged-in user.
+
+    Retrieves the specified recipe by ID if it belongs to the current user. 
+    If the request method is POST, this view updates the recipe with the 
+    provided form data. If the form is valid, it saves the changes, displays 
+    a success message, and redirects to the dashboard view. If the request 
+    method is not POST, the recipe form is displayed with the current recipe 
+    data for editing.
+
+    Parameters:
+    request (HttpRequest): The HTTP request object containing metadata about the request.
+    recipe_id (int): The ID of the recipe to edit.
+
+    Returns:
+    HttpResponse: If the request method is GET, renders the recipe edit template with 
+                  the current recipe data. If the form is submitted and valid (POST), 
+                  redirects to the dashboard view after saving the recipe.
+
+    Note:
+    The user must be logged in to access this view.
+    """
     recipe = get_object_or_404(Recipe, id=recipe_id, user_id=request.user.id)
     if request.method == 'POST':
         form = forms.RecipeForm(request.POST, instance=recipe)
@@ -74,6 +117,23 @@ def Edit(request, recipe_id):
 
 @login_required
 def Delete(request, recipe_id):
+    """
+    Handles the deletion of a recipe for the currently logged-in user.
+
+    Retrieves the specified recipe by ID if it belongs to the current user 
+    and deletes it. After successful deletion, a success message is displayed, 
+    and the user is redirected to the dashboard view.
+
+    Parameters:
+    request (HttpRequest): The HTTP request object containing metadata about the request.
+    recipe_id (int): The ID of the recipe to delete.
+
+    Returns:
+    HttpResponse: Redirects to the dashboard view after deleting the recipe.
+
+    Note:
+    The user must be logged in to access this view.
+    """
     recipe = get_object_or_404(Recipe, id=recipe_id, user_id=request.user.id)
     recipe.delete()
     messages.success(
@@ -82,5 +142,21 @@ def Delete(request, recipe_id):
 
 
 def Show(request, recipe_id):
+    """
+    Displays the details of a specific recipe for the currently logged-in user.
+
+    Retrieves the specified recipe by ID if it belongs to the current user and 
+    renders it in the recipe detail template.
+
+    Parameters:
+    request (HttpRequest): The HTTP request object containing metadata about the request.
+    recipe_id (int): The ID of the recipe to display.
+
+    Returns:
+    HttpResponse: Renders the recipe detail template with the selected recipe data.
+
+    Note:
+    The user must be logged in to access this view.
+    """
     recipe = get_object_or_404(Recipe, id=recipe_id, user_id=request.user.id)
     return render(request, 'recipe/dashboard/show.html', {'recipe': recipe})
